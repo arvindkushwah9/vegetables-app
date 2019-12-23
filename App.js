2fb1d3fcd7937bed087b2215879054e0756eafd1
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-12-23 11:07:47 +0530

    Fixed issue

--- App.js
+++ App.js
@@ -4,13 +4,6 @@ import Routes from './src/Router/Routes.js';
 import SplashScreen from 'react-native-splash-screen';
 import FlashMessage from "react-native-flash-message";
 import { genericStyles } from './src/Styles/genericStyles'
-import { ContentView } from './src/Components/ContentView'
-
-import Menu from 'react-native-side-menu';
-
-const SideMenu = require('react-native-side-menu');
-const menu = <Menu navigator={navigator}/>;
-
 
 class App extends Component {
 
@@ -23,10 +16,7 @@ class App extends Component {
       return ( 
         <View style={genericStyles.container}>     
         <Routes />
-        <FlashMessage position="top" />
-        <SideMenu menu={menu}>
-        <ContentView/>
-      </SideMenu>
+        <FlashMessage position="top" />        
        </View>
       )
    }
